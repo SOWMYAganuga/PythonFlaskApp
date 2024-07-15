d0b31b49caeb57776109b830611ba36e4eff37c9
Sowmya Ganuga <sowmyaganuga956@gmail.com>
2024-07-15 10:15:54 -0400

    Initial commit

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flaskapp',
+    name='PythonFlaskApp',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Flask',
+        # Add other dependencies from your requirements.txt here
     ],
 )
