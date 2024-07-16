b66f7a375b8a491e1666e80c71bf167f5f70fbf3
Sowmya Ganuga <sowmyaganuga956@gmail.com>
2024-07-16 14:15:53 -0400

    Initial commit

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='PythonFlaskApp',
-    version='1.0',
+    version='0.1',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
-        'Flask',
-        # Add other dependencies from your requirements.txt here
+        'Flask>=2.0',
+        # Add other dependencies here
     ],
+    entry_points={
+        'console_scripts': [
+            'runapp=app:main',  # Adjust this to your app's entry point
+        ],
+    },
 )
