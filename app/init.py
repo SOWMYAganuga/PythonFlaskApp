33be9184cd3f0d93a38f9407b3660554b5b6904a
Sowmya Ganuga <sowmyaganuga956@gmail.com>
2024-07-17 10:06:06 -0400

    Initial commit

--- app/init.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask # type: ignore
-
-app = Flask(__name__)
-
-from app import routes
--- app/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template # type: ignore
-from app import app
-
-@app.route('/')
-def index():
-    return render_template('index.html', message="Hello, World!")
